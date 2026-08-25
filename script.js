9936762bf44902dbaa5eedff25f199fce10c3118
Anmolbabbar <anmolbabbar877@gmail.com>
2026-08-25 13:17:57 +0530

    updated 9

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-// javascript
-//REGISTER BUTTON
-const registerButtons = document.querySelectorAll(".register-button, .secondary.register-main-button");
-button.addEventListener("click", function(event){
-    event.preventDefault();
-    alert("Registeration for NIRVAN'26 is opening soon!");s
-
+//javascript
+// 1. REGISTER BUTTON
+const registerButtons = document.querySelectorAll(".register-button, .secondary, .register-main-button");
+registerButtons.forEach(function(button){
+    button.addEventListener("click", function(event) {
+        event.preventDefault();
+        alert("Registration is opening soon!");
+    });
 });
-//EVENT DETAILS BUTTONS
-const eventButtons= document.querySelectorAll(".event-button");
-eventButtons.forEach(function(button))
-{
-    button.addEventListener("click",function());
-    {
-        const eventName= this.parrentElement.querySelector("h3").innerText;
-        alert("You selected:" + eventName);
-    }
-}
+// 2. EVENT BUTTONS
+const eventButtons = document.querySelectorAll(".event-card button");
+eventButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        const eventName =
+            this.parentElement.querySelector("h3").innerText;
+        alert("You selected: " + eventName +"\n\nMore details coming soon!");
+    });
+});
+//3.
